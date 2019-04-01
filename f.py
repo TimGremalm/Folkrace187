651fcefd4a04adb83fcc34329589b2fb74258f84
Tim Gremalm <tim@gremalm.se>
2019-04-01 02:39:27 +0200

    Added support for vl53l0x

--- f.py
+++ f.py
@@ -2,6 +2,13 @@
 
 print('Hello')
 
+import machine
+#D0 = GPIO16, D1 = GPIO5 (https://github.com/nodemcu/nodemcu-devkit-v1.0?ts=4#pin-map)
+i = machine.I2C(scl=machine.Pin(16), sda=machine.Pin(5))
+
+import vl53l0x
+v = vl53l0x.VL53L0X(i)
+v.read()
 
 def reload():
 	import sys
